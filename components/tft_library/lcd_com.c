16283e7bf0b5fe1d961fe7340d2e82b9f115af00
yac123-maker <yacinoss123@gmail.com>
2024-12-07 02:21:44 +0100

    first commit

---
+++ components/tft_library/lcd_com.c
@@ -0,0 +1,603 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "soc/soc.h"
+#include "soc/dport_reg.h"
+#include "lcd_com.h"
+#include "i2s_lcd_driver.h"
+#include "hal/gpio_ll.h" // idf-py ver5
+#include "driver/gpio.h"
+//#include "driver/adc_common.h"
+
+#define TAG "LCD_COM"
+
+#if CONFIG_CUSTOM_GPIO_CONFIG
+#define LCD_D0_PIN	(12)
+#define LCD_D1_PIN	(13)
+#define LCD_D2_PIN	(26)
+#define LCD_D3_PIN	(25)
+#define LCD_D4_PIN	(17)
+#define LCD_D5_PIN	(16)
+#define LCD_D6_PIN	(27)
+#define LCD_D7_PIN	(14)
+#define LCD_RD_PIN	(2)
+#define LCD_WR_PIN	(4)
+#define LCD_RS_PIN	(15)
+#define LCD_CS_PIN	(23)
+#define LCD_RESET_PIN (22)
+#else
+#ifdef CONFIG_IDF_TARGET_ESP32
+#define LCD_D0_PIN	(12)
+#define LCD_D1_PIN	(13)
+#define LCD_D2_PIN	(26)
+#define LCD_D3_PIN	(25)
+#define LCD_D4_PIN	(17)
+#define LCD_D5_PIN	(16)
+#define LCD_D6_PIN	(27)
+#define LCD_D7_PIN	(14)
+#define LCD_RD_PIN	(2)
+#define LCD_WR_PIN	(4)
+#define LCD_RS_PIN	(15)
+#define LCD_CS_PIN	(23)
+#define LCD_RESET_PIN (22)
+#elif defined CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32S3
+#define LCD_D0_PIN	(1)
+#define LCD_D1_PIN	(2)
+#define LCD_D2_PIN	(3)
+#define LCD_D3_PIN	(4)
+#define LCD_D4_PIN	(5)
+#define LCD_D5_PIN	(6)
+#define LCD_D6_PIN	(7) // (11)
+#define LCD_D7_PIN	(8) // (12)
+#define LCD_RD_PIN	(39)
+#define LCD_WR_PIN	(40)
+#define LCD_RS_PIN	(41)
+#define LCD_CS_PIN	(42)
+#define LCD_RESET_PIN (45)
+#endif
+#endif
+
+#define gpio_digital_write(GPIO_PIN, data) \
+	do { \
+		if (data) { \
+			gpio_set_level( GPIO_PIN, 1 ); \
+		} else { \
+			gpio_set_level( GPIO_PIN, 0 ); \
+		} \
+	} while (0)
+	
+#define reg_digital_write(GPIO_PIN, data) \
+	do { \
+		if (data) { \
+			if ( GPIO_PIN < 32 ) GPIO.out_w1ts = (1 << GPIO_PIN); \
+			else GPIO.out1_w1ts.val = (1 << (GPIO_PIN - 32)); \
+		} else { \
+			if ( GPIO_PIN < 32 ) GPIO.out_w1tc = (1 << GPIO_PIN); \
+			else GPIO.out1_w1tc.val = (1 << (GPIO_PIN - 32)); \
+		} \
+	} while (0)
+
+void gpio_lcd_write_data(int dummy1, unsigned char *data, size_t size) {
+	for (int i=0;i<size;i++) {
+		gpio_digital_write(LCD_D0_PIN, data[i] & 0x01);
+		gpio_digital_write(LCD_D1_PIN, data[i] & 0x02);
+		gpio_digital_write(LCD_D2_PIN, data[i] & 0x04);
+		gpio_digital_write(LCD_D3_PIN, data[i] & 0x08);
+		gpio_digital_write(LCD_D4_PIN, data[i] & 0x10); 
+		gpio_digital_write(LCD_D5_PIN, data[i] & 0x20);
+		gpio_digital_write(LCD_D6_PIN, data[i] & 0x40);
+		gpio_digital_write(LCD_D7_PIN, data[i] & 0x80);  
+		gpio_set_level( LCD_WR_PIN, 0 );
+		gpio_set_level( LCD_WR_PIN, 1 );
+	}
+}
+
+void reg_lcd_write_data(int dummy1, unsigned char *data, size_t size) {
+	for (int i=0;i<size;i++) {
+		reg_digital_write(LCD_D0_PIN, data[i] & 0x01);
+		reg_digital_write(LCD_D1_PIN, data[i] & 0x02);
+		reg_digital_write(LCD_D2_PIN, data[i] & 0x04);
+		reg_digital_write(LCD_D3_PIN, data[i] & 0x08);
+		reg_digital_write(LCD_D4_PIN, data[i] & 0x10); 
+		reg_digital_write(LCD_D5_PIN, data[i] & 0x20);
+		reg_digital_write(LCD_D6_PIN, data[i] & 0x40);
+		reg_digital_write(LCD_D7_PIN, data[i] & 0x80);	
+		gpio_set_level( LCD_WR_PIN, 0 );
+		gpio_set_level( LCD_WR_PIN, 1 );
+	}
+}
+
+
+void lcd_write_table(TFT_t * dev, const void *table, int16_t size)
+{
+	int i;
+	uint8_t *p = (uint8_t *) table;
+	while (size > 0) {
+		uint8_t cmd = *p++;
+		uint8_t len = *p++;
+		if (cmd == TFTLCD_DELAY8) {
+			lcd_delay_ms(len);
+			len = 0;
+		} else {
+			lcd_write_comm_byte(dev, cmd );
+			for (i = 0; i < len; i++) {
+				uint8_t data = *p++;
+				lcd_write_data_byte(dev, data );
+			}
+		}
+		size -= len + 2;
+	}
+}
+
+void lcd_write_table16(TFT_t * dev, const void *table, int16_t size)
+{
+	uint16_t *p = (uint16_t *) table;
+	while (size > 0) {
+		uint16_t cmd = *p++;
+		uint16_t dat = *p++;
+		if (cmd == TFTLCD_DELAY16)
+			lcd_delay_ms(dat);
+		else {
+			lcd_write_register_word(dev, cmd, dat);
+		}
+		size -= 2 * sizeof(int16_t);
+	}
+}
+
+
+void lcd_write_comm_byte(TFT_t * dev, uint8_t cmd)
+{
+	unsigned char c[1];
+	c[0] = cmd;
+
+	gpio_set_level(dev->_cs, 0);
+	gpio_set_level(dev->_rs, 0);
+	//gpio_set_level(dev->_rd, 1);
+	if (dev->_interface == INTERFACE_I2S) {
+		i2s_lcd_write(dev->i2s_lcd_handle, c, 1);
+	} else if (dev->_interface == INTERFACE_GPIO) {
+		gpio_lcd_write_data(GPIO_PORT_NUM, c, 1);
+	} else if (dev->_interface == INTERFACE_REG) {
+		reg_lcd_write_data(GPIO_PORT_NUM, c, 1);
+	}
+	gpio_set_level(dev->_cs, 1);
+	if (dev->_delay != 0) esp_rom_delay_us(dev->_delay);
+}
+
+void lcd_write_comm_word(TFT_t * dev, uint16_t cmd)
+{
+	unsigned char c[2];
+	c[0] = (cmd >> 8) & 0xFF;
+	c[1] = cmd & 0xFF;
+
+	gpio_set_level(dev->_cs, 0);
+	gpio_set_level(dev->_rs, 0);
+	//gpio_set_level(dev->_rd, 1);
+	if (dev->_interface == INTERFACE_I2S) {
+		i2s_lcd_write(dev->i2s_lcd_handle, c, 2);
+	} else if (dev->_interface == INTERFACE_GPIO) {
+		gpio_lcd_write_data(GPIO_PORT_NUM, c, 2);
+	} else if (dev->_interface == INTERFACE_REG) {
+		reg_lcd_write_data(GPIO_PORT_NUM, c, 2);
+	}
+	gpio_set_level(dev->_cs, 1);
+	if (dev->_delay != 0) esp_rom_delay_us(dev->_delay);
+}
+
+void lcd_write_data_byte(TFT_t * dev, uint8_t data)
+{
+	unsigned char d[1];
+	d[0] = data;
+
+	gpio_set_level(dev->_cs, 0);
+	gpio_set_level(dev->_rs, 1);
+	//gpio_set_level(dev->_rd, 1);
+	if (dev->_interface == INTERFACE_I2S) {
+		i2s_lcd_write(dev->i2s_lcd_handle, d, 1);
+	} else if (dev->_interface == INTERFACE_GPIO) {
+		gpio_lcd_write_data(GPIO_PORT_NUM, d, 1);
+	} else if (dev->_interface == INTERFACE_REG) {
+		reg_lcd_write_data(GPIO_PORT_NUM, d, 1);
+	}
+	gpio_set_level(dev->_cs, 1);
+	if (dev->_delay != 0) esp_rom_delay_us(dev->_delay);
+}
+
+
+void lcd_write_data_word(TFT_t * dev, uint16_t data)
+{
+	unsigned char d[2];
+	d[0] = (data >> 8) & 0xFF;
+	d[1] = data & 0xFF;
+
+	gpio_set_level(dev->_cs, 0);
+	gpio_set_level(dev->_rs, 1);
+	//gpio_set_level(dev->_rd, 1);
+	if (dev->_interface == INTERFACE_I2S) {
+		i2s_lcd_write(dev->i2s_lcd_handle, d, 2);
+	} else if (dev->_interface == INTERFACE_GPIO) {
+		gpio_lcd_write_data(GPIO_PORT_NUM, d, 2);
+	} else if (dev->_interface == INTERFACE_REG) {
+		reg_lcd_write_data(GPIO_PORT_NUM, d, 2);
+	}
+	gpio_set_level(dev->_cs, 1);
+	if (dev->_delay != 0) esp_rom_delay_us(dev->_delay);
+}
+
+void lcd_write_addr(TFT_t * dev, uint16_t addr1, uint16_t addr2)
+{
+	unsigned char c[4];
+	c[0] = (addr1 >> 8) & 0xFF;
+	c[1] = addr1 & 0xFF;
+	c[2] = (addr2 >> 8) & 0xFF;
+	c[3] = addr2 & 0xFF;
+	//ESP_LOGI(__FUNCTION__, "c=%02x-%02x-%02x-%02x",c[0],c[1],c[2],c[3]);
+
+	gpio_set_level(dev->_cs, 0);
+	gpio_set_level(dev->_rs, 1);
+	//gpio_set_level(dev->_rd, 1);
+	if (dev->_interface == INTERFACE_I2S) {
+		i2s_lcd_write(dev->i2s_lcd_handle, c, 4);
+	} else if (dev->_interface == INTERFACE_GPIO) {
+		gpio_lcd_write_data(GPIO_PORT_NUM, c, 4);
+	} else if (dev->_interface == INTERFACE_REG) {
+		reg_lcd_write_data(GPIO_PORT_NUM, c, 4);
+	}
+	gpio_set_level(dev->_cs, 1);
+	if (dev->_delay != 0) esp_rom_delay_us(dev->_delay);
+}
+
+void lcd_write_color(TFT_t * dev, uint16_t color, uint16_t size)
+{
+	unsigned char *data;
+	if ((data = malloc(size * 2)) == NULL) return;
+	int index = 0;
+	for(int i=0;i<size;i++) {
+		data[index++] = (color >> 8) & 0xFF;
+		data[index++] = color & 0xFF;
+	}
+
+	gpio_set_level(dev->_cs, 0);
+	gpio_set_level(dev->_rs, 1);
+	//gpio_set_level(dev->_rd, 1);
+	if (dev->_interface == INTERFACE_I2S) {
+		i2s_lcd_write(dev->i2s_lcd_handle, data, size*2);
+	} else if (dev->_interface == INTERFACE_GPIO) {
+		gpio_lcd_write_data(GPIO_PORT_NUM, data, size*2);
+	} else if (dev->_interface == INTERFACE_REG) {
+		reg_lcd_write_data(GPIO_PORT_NUM, data, size*2);
+	}
+	gpio_set_level(dev->_cs, 1);
+	free(data);
+	if (dev->_delay != 0) esp_rom_delay_us(dev->_delay);
+}
+
+void lcd_write_colors(TFT_t * dev, uint16_t * colors, uint16_t size)
+{
+	unsigned char *data;
+	if ((data = malloc(size * 2)) == NULL) return;
+	int index = 0;
+	for(int i=0;i<size;i++) {
+		data[index++] = (colors[i] >> 8) & 0xFF;
+		data[index++] = colors[i] & 0xFF;
+	}
+
+	gpio_set_level(dev->_cs, 0);
+	gpio_set_level(dev->_rs, 1);
+	//gpio_set_level(dev->_rd, 1);
+	if (dev->_interface == INTERFACE_I2S) {
+		i2s_lcd_write(dev->i2s_lcd_handle, data, size*2);
+	} else if (dev->_interface == INTERFACE_GPIO) {
+		gpio_lcd_write_data(GPIO_PORT_NUM, data, size*2);
+	} else if (dev->_interface == INTERFACE_REG) {
+		reg_lcd_write_data(GPIO_PORT_NUM, data, size*2);
+	}
+	gpio_set_level(dev->_cs, 1);
+	free(data);
+	if (dev->_delay != 0) esp_rom_delay_us(dev->_delay);
+}
+
+void lcd_delay_ms(int delay_time)
+{
+	vTaskDelay(delay_time/portTICK_PERIOD_MS);
+}
+
+void lcd_write_register_word(TFT_t * dev, uint16_t addr, uint16_t data)
+{
+	lcd_write_comm_word(dev, addr);
+	lcd_write_data_word(dev, data);
+}
+
+void lcd_write_register_byte(TFT_t * dev, uint8_t addr, uint16_t data)
+{
+	lcd_write_comm_byte(dev, addr);
+	lcd_write_data_word(dev, data);
+}
+
+#define BOARD_LCD_I2S_BITWIDTH 8
+
+esp_err_t lcd_interface_cfg(TFT_t * dev, int interface)
+{
+	ESP_LOGI(TAG, "LCD_CS_PIN=%d",LCD_CS_PIN);
+	gpio_reset_pin( LCD_CS_PIN );
+	gpio_set_direction( LCD_CS_PIN, GPIO_MODE_OUTPUT );
+	gpio_set_level( LCD_CS_PIN, 1 );
+
+	ESP_LOGI(TAG, "LCD_RS_PIN=%d",LCD_RS_PIN);
+	gpio_reset_pin( LCD_RS_PIN );
+	gpio_set_direction(LCD_RS_PIN, GPIO_MODE_OUTPUT);
+	gpio_set_level( LCD_RS_PIN, 1 );
+
+	ESP_LOGI(TAG, "LCD_WR_PIN=%d",LCD_WR_PIN);
+	gpio_reset_pin( LCD_WR_PIN );
+	gpio_set_direction( LCD_WR_PIN, GPIO_MODE_OUTPUT );
+	gpio_set_level( LCD_WR_PIN, 1 );
+
+	ESP_LOGI(TAG, "LCD_RD_PIN=%d",LCD_RD_PIN);
+	gpio_reset_pin( LCD_RD_PIN );
+	gpio_set_direction( LCD_RD_PIN, GPIO_MODE_OUTPUT );
+	gpio_set_level( LCD_RD_PIN, 1 );
+
+	ESP_LOGI(TAG, "LCD_D0_PIN=%d",LCD_D0_PIN);
+	ESP_LOGI(TAG, "LCD_D1_PIN=%d",LCD_D1_PIN);
+	ESP_LOGI(TAG, "LCD_D2_PIN=%d",LCD_D2_PIN);
+	ESP_LOGI(TAG, "LCD_D3_PIN=%d",LCD_D3_PIN);
+	ESP_LOGI(TAG, "LCD_D4_PIN=%d",LCD_D4_PIN);
+	ESP_LOGI(TAG, "LCD_D5_PIN=%d",LCD_D5_PIN);
+	ESP_LOGI(TAG, "LCD_D6_PIN=%d",LCD_D6_PIN);
+	ESP_LOGI(TAG, "LCD_D7_PIN=%d",LCD_D7_PIN);
+
+	if (interface == INTERFACE_I2S) {
+		ESP_LOGI(TAG, "INTERFACE is I2S");
+		i2s_lcd_config_t i2s_lcd_cfg = {
+			.data_width  = BOARD_LCD_I2S_BITWIDTH,
+			.pin_data_num = {
+				LCD_D0_PIN,
+				LCD_D1_PIN,
+				LCD_D2_PIN,
+				LCD_D3_PIN,
+				LCD_D4_PIN,
+				LCD_D5_PIN,
+				LCD_D6_PIN,
+				LCD_D7_PIN,
+				//BOARD_LCD_I2S_D8_PIN,
+				//BOARD_LCD_I2S_D9_PIN,
+				//BOARD_LCD_I2S_D10_PIN,
+				//BOARD_LCD_I2S_D11_PIN,
+				//BOARD_LCD_I2S_D12_PIN,
+				//BOARD_LCD_I2S_D13_PIN,
+				//BOARD_LCD_I2S_D14_PIN,
+				//BOARD_LCD_I2S_D15_PIN,
+			},
+			.pin_num_cs = LCD_CS_PIN,
+			.pin_num_wr = LCD_WR_PIN,
+			.pin_num_rs = LCD_RS_PIN,
+	
+			.clk_freq = 20000000,
+			.i2s_port = I2S_NUM_0,
+			.buffer_size = 32000,
+			.swap_data = false,
+		};
+
+
+		//i2s_lcd_handle_t i2s_lcd_handle;
+		dev->i2s_lcd_handle = i2s_lcd_driver_init(&i2s_lcd_cfg);
+		if (NULL == dev->i2s_lcd_handle) {
+			ESP_LOGE(TAG, "%s:%d (%s):%s", __FILE__, __LINE__, __FUNCTION__, "screen 8080 interface create failed");
+			return ESP_FAIL;
+		}
+
+	} else if (interface == INTERFACE_GPIO || interface == INTERFACE_REG) {
+		if (interface == INTERFACE_GPIO) {
+			ESP_LOGI(TAG, "INTERFACE is GPIO");
+		} else {
+			ESP_LOGI(TAG, "INTERFACE is REGISTER I/O");
+		}
+		gpio_reset_pin( LCD_D0_PIN );
+		gpio_reset_pin( LCD_D1_PIN );
+		gpio_reset_pin( LCD_D2_PIN );
+		gpio_reset_pin( LCD_D3_PIN );
+		gpio_reset_pin( LCD_D4_PIN );
+		gpio_reset_pin( LCD_D5_PIN );
+		gpio_reset_pin( LCD_D6_PIN );
+		gpio_reset_pin( LCD_D7_PIN );
+		gpio_set_direction( LCD_D0_PIN, GPIO_MODE_OUTPUT );
+		gpio_set_direction( LCD_D1_PIN, GPIO_MODE_OUTPUT );
+		gpio_set_direction( LCD_D2_PIN, GPIO_MODE_OUTPUT );
+		gpio_set_direction( LCD_D3_PIN, GPIO_MODE_OUTPUT );
+		gpio_set_direction( LCD_D4_PIN, GPIO_MODE_OUTPUT );
+		gpio_set_direction( LCD_D5_PIN, GPIO_MODE_OUTPUT );
+		gpio_set_direction( LCD_D6_PIN, GPIO_MODE_OUTPUT );
+		gpio_set_direction( LCD_D7_PIN, GPIO_MODE_OUTPUT );
+		dev->_d0 = LCD_D0_PIN;
+		dev->_d1 = LCD_D1_PIN;
+		dev->_d2 = LCD_D2_PIN;
+		dev->_d3 = LCD_D3_PIN;
+		dev->_d4 = LCD_D4_PIN;
+		dev->_d5 = LCD_D5_PIN;
+		dev->_d6 = LCD_D6_PIN;
+		dev->_d7 = LCD_D7_PIN;
+	}
+
+	ESP_LOGI(TAG, "LCD_RESET_PIN=%d",LCD_RESET_PIN);
+	gpio_reset_pin( LCD_RESET_PIN );
+	gpio_set_direction( LCD_RESET_PIN, GPIO_MODE_OUTPUT );
+	gpio_set_level( LCD_RESET_PIN, 1 );
+	vTaskDelay( pdMS_TO_TICKS( 100 ) );
+	gpio_set_level( LCD_RESET_PIN, 0 );
+	vTaskDelay( pdMS_TO_TICKS( 100 ) );
+	gpio_set_level( LCD_RESET_PIN, 1 );
+
+	dev->_rd = LCD_RD_PIN;
+	dev->_wr = LCD_WR_PIN;
+	dev->_rs = LCD_RS_PIN;
+	dev->_cs = LCD_CS_PIN;
+	dev->_interface = interface;
+	
+	return ESP_OK;
+}
+
+void touch_interface_cfg(TFT_t * dev, int adc_yp, int adc_xm, int gpio_xp, int gpio_xm, int gpio_yp, int gpio_ym)
+{
+	dev->_calibration = true;
+	dev->_adc_yp = adc_yp;
+	dev->_adc_xm = adc_xm;
+	dev->_gpio_xp = gpio_xp;
+	dev->_gpio_xm = gpio_xm;
+	dev->_gpio_yp = gpio_yp;
+	dev->_gpio_ym = gpio_ym;
+		ESP_LOGI(TAG, "_adc_yp=%d _adc_xm=%d", dev->_adc_yp, dev->_adc_xm);
+	ESP_LOGI(TAG, "_gpio_xp=%d _gpio_xm=%d", gpio_xp, gpio_xm);
+	ESP_LOGI(TAG, "_gpio_yp=%d _gpio_ym=%d", gpio_yp, gpio_ym);
+	ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+	ESP_ERROR_CHECK(adc1_config_channel_atten(dev->_adc_yp, ADC_ATTEN_DB_12));
+	ESP_ERROR_CHECK(adc1_config_channel_atten(dev->_adc_xm, ADC_ATTEN_DB_12));
+#else
+	ESP_ERROR_CHECK(adc1_config_channel_atten(dev->_adc_yp, ADC_ATTEN_DB_11));
+	ESP_ERROR_CHECK(adc1_config_channel_atten(dev->_adc_xm, ADC_ATTEN_DB_11));
+#endif
+}
+
+int touch_avr_analog(adc1_channel_t channel, int averagetime)
+{
+	if (averagetime > 2) {
+		int sum = 0;
+		int max = 0;
+		//int min = 1024;
+		int min = INT_MAX;
+		for(int i = 0; i<averagetime; i++)
+		{
+			int adc_raw = adc1_get_raw(channel);
+			//printf("adc_raw=%d\n",adc_raw);
+			if(adc_raw > max)max = adc_raw;
+			if(adc_raw < min)min = adc_raw;
+			sum += adc_raw;
+			//sum+=analogRead(adpin);
+		}
+		// A simple filter to exclude min and max values
+		// adc_row[0]=100;
+		// adc_row[1]=110;
+		// adc_row[2]=120;
+		// adc_row[3]=130;
+		// sum=100+110+120+130=460
+		// min=100
+		// max=130
+		// (460-100-130)/2=115
+		int a=(sum-min-max)/(averagetime-2);
+
+		return a;
+	} else {
+		return 0;
+	}
+}
+
+void touch_gpio(int gpio, int mode, int level)
+{
+	esp_log_level_set("gpio", ESP_LOG_WARN);
+	gpio_reset_pin(gpio);
+	gpio_config_t io_conf = {};
+	io_conf.pin_bit_mask = (1ULL<<gpio);
+	if (mode == MODE_OUTPUT) {
+		io_conf.mode = GPIO_MODE_OUTPUT;
+		ESP_ERROR_CHECK(gpio_config(&io_conf));
+		gpio_set_level(gpio, level);
+	} else if(mode == MODE_INPUT) {
+		io_conf.mode = GPIO_MODE_INPUT;
+		io_conf.intr_type = GPIO_INTR_DISABLE;
+		io_conf.pull_down_en = GPIO_PULLDOWN_ENABLE;
+		ESP_ERROR_CHECK(gpio_config(&io_conf));
+	}
+}
+
+// Get X coordinate from 4-line resistance touch screen
+// Read the analog value twice from Y{+}
+// X=valueY{+}[0]+valueY{+}[1]
+int touch_getx(TFT_t * dev)
+{
+	int xp = 0;
+	touch_gpio(dev->_gpio_yp, MODE_INPUT, 0);
+	touch_gpio(dev->_gpio_ym, MODE_INPUT, 0);
+	touch_gpio(dev->_gpio_xp, MODE_OUTPUT, 1);
+	touch_gpio(dev->_gpio_xm, MODE_OUTPUT, 0);
+
+	int samples[NUM_SAMPLES];
+	for (int i=0; i<NUM_SAMPLES; i++) {
+		samples[i] = touch_avr_analog(dev->_adc_yp, AVERAGE_TIME);
+
+	}
+	int icomp =  samples[0] > samples[1]? samples[0] - samples[1]: samples[1] -  samples[0];
+	ESP_LOGD(TAG, "touch_getx adc=%d samples[0]=%d samples[1]=%d icomp=%d COMP_TOLERANCE=%d", dev->_adc_yp, samples[0], samples[1], icomp, COMP_TOLERANCE);
+	if(icomp <= COMP_TOLERANCE) xp = samples[0] + samples[1];
+	return xp;
+}
+
+// Get Y coordinate from 4-line resistance touch screen
+// Read the analog value twice from X{-}s
+// Y=valueX{-}[0]+valueX{-}[1]
+int touch_gety(TFT_t * dev)
+{
+	int yp = 0;
+	touch_gpio(dev->_gpio_yp, MODE_OUTPUT, 1);
+	touch_gpio(dev->_gpio_ym, MODE_OUTPUT, 0);
+	touch_gpio(dev->_gpio_xp, MODE_INPUT, 0);
+	touch_gpio(dev->_gpio_xm, MODE_INPUT, 0);
+
+	int samples[NUM_SAMPLES];
+	for (int i=0; i<NUM_SAMPLES; i++) {
+		samples[i] = touch_avr_analog(dev->_adc_xm, AVERAGE_TIME);
+
+	}
+	int icomp =  samples[0] > samples[1]? samples[0] - samples[1]: samples[1] -  samples[0];
+	ESP_LOGD(TAG, "adc=%d samples[0]=%d samples[1]=%d icomp=%d COMP_TOLERANCE=%d", dev->_adc_xm, samples[0], samples[1], icomp, COMP_TOLERANCE);
+	if(icomp <= COMP_TOLERANCE) yp = samples[0] + samples[1];
+	return yp;
+
+}
+
+// Get Z value from 4-line resistance touch screen
+// Read the analog value from Y{+}
+// Read the analog value from X{-}
+// Z=ABS(valueY{+}-valueX{-})
+int touch_getz(TFT_t * dev)
+{
+	touch_gpio(dev->_gpio_yp, MODE_INPUT, 0);
+	touch_gpio(dev->_gpio_ym, MODE_OUTPUT, 1);
+	touch_gpio(dev->_gpio_xp, MODE_OUTPUT, 0);
+	touch_gpio(dev->_gpio_xm, MODE_INPUT, 0);
+
+	int z1 = adc1_get_raw(dev->_adc_yp);
+	int z2 = adc1_get_raw(dev->_adc_xm);
+
+	int icomp =  z1 > z2? z1 - z2: z2 -  z1;
+	ESP_LOGD(TAG, "z1=%d z2=%d icomp=%d", z1, z2, icomp);
+	return icomp;
+}
+
+void touch_getxyz(TFT_t * dev, int *xp, int *yp, int *zp)
+{
+	*xp = touch_getx(dev);
+	*yp = touch_gety(dev);
+	*zp = touch_getz(dev);
+	touch_gpio(dev->_gpio_yp, MODE_OUTPUT, 0);
+	touch_gpio(dev->_gpio_ym, MODE_OUTPUT, 0);
+	touch_gpio(dev->_gpio_xp, MODE_OUTPUT, 0);
+	touch_gpio(dev->_gpio_xm, MODE_OUTPUT, 0);
+}
+
+bool touch_getxy(TFT_t *dev, int *xp, int *yp)
+{
+	int _xp;
+	int _yp;
+	int _zp;
+	touch_getxyz(dev, &_xp, &_yp, &_zp);
+	if (_xp == 0) return false;
+	if (_yp == 0) return false;
+	if (_zp == 1023) return false;
+	*xp = _xp;
+	*yp = _yp;
+	return true;
+}	
---
+++ main/main.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <math.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "nvs.h"
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
+
+#include "lcd_com.h"
+#include "lcd_lib.h"
+#include "fontx.h"
+#include "bmpfile.h"
+#include "decode_jpeg.h"
+#include "decode_png.h"
+#include "pngle.h"
+#include "fonctions.c"
+
+
+
+void TFT(void *pvParameters)
+{
+	// IMPORTER LES FONTS
+
+
+	FontxFile fx16G[2];
+	FontxFile fx24G[2];
+	FontxFile fx32G[2];
+	InitFontx(fx16G,"/spiffs/ILGH16XB.FNT",""); // 8x16Dot Gothic
+	InitFontx(fx24G,"/spiffs/ILGH24XB.FNT",""); // 12x24Dot Gothic
+	InitFontx(fx32G,"/spiffs/ILGH32XB.FNT",""); // 16x32Dot Gothic
+
+	FontxFile fx32L[2];
+	InitFontx(fx32L,"/spiffs/LATIN32B.FNT",""); // 16x32Dot Latinc
+
+	FontxFile fx16M[2];
+	FontxFile fx24M[2];
+	FontxFile fx32M[2];
+	InitFontx(fx16M,"/spiffs/ILMH16XB.FNT",""); // 8x16Dot Mincyo
+	InitFontx(fx24M,"/spiffs/ILMH24XB.FNT",""); // 12x24Dot Mincyo
+	InitFontx(fx32M,"/spiffs/ILMH32XB.FNT",""); // 16x32Dot Mincyo
+	// IMPORTATION FINIE
+	
+	//INITIALISER LE PROTOCOL DE COMMUNICATION DE L'ECRAN GRAPHIQUE, DANS NOTRE CAS, COMMUNICATION A REGISTRE
+	TFT_t dev;
+	lcd_interface_cfg(&dev, INTERFACE);
+
+	//PARAMETRES DE L'ECRAN HAUTER/LARGEUR
+	INIT_FUNCTION(&dev, CONFIG_WIDTH, CONFIG_HEIGHT, CONFIG_OFFSETX, CONFIG_OFFSETY);
+
+
+// INVERSION DES COULEURS OUi/NON
+
+#if CONFIG_INVERSION
+	ESP_LOGI(TAG, "Enable Display Inversion");
+	lcdInversionOn(&dev);
+#endif
+
+
+
+/*
+Signal analogique pour le tactile * L'ECRAN NE MARCHE PAS *
+X+ X- Y+ Y- SEULEMENT X+ ET X- MARCHENT
+*/
+
+#if CONFIG_ENABLE_TOUCH
+
+#if CONFIG_XP_GPIO_D6
+  int gpio_xp = dev._d0;
+#elif CONFIG_XP_GPIO_D0
+  int gpio_xp = dev._d0;
+#elif CONFIG_XP_GPIO_D1
+  int gpio_xp = dev._d1;
+#endif
+
+#if CONFIG_XM_GPIO_RS
+  int gpio_xm = dev._rs;
+#elif CONFIG_XM_GPIO_CS
+  int gpio_xm = dev._cs;
+#endif
+
+#if CONFIG_YP_GPIO_WR
+  int gpio_yp = dev._cs;
+#elif CONFIG_YP_GPIO_CS
+  int gpio_yp = dev._cs;
+#elif CONFIG_YP_GPIO_RS
+  int gpio_yp = dev._rs;
+#endif
+
+#if CONFIG_YM_GPIO_D7
+  int gpio_ym = dev._d1;
+#elif CONFIG_YM_GPIO_D0
+  int gpio_ym = dev._d0;
+#elif CONFIG_YM_GPIO_D1
+  int gpio_ym = dev._d1;
+#endif
+	touch_interface_cfg(&dev, CONFIG_ADC_CHANNEL_YP, CONFIG_ADC_CHANNEL_XM, gpio_xp, gpio_xm, gpio_yp, gpio_ym);
+#endif
+
+
+
+	while(1) {
+		traceHeap();
+
+#if  0 //CONFIG_ENABLE_TOUCH
+
+		TouchCalibration(&dev, fx24G, CONFIG_WIDTH, CONFIG_HEIGHT);
+		TouchPenTest(&dev, fx24G, CONFIG_WIDTH, CONFIG_HEIGHT, 1000);
+
+		TouchKeyTest(&dev, fx32G, CONFIG_WIDTH, CONFIG_HEIGHT, 1000);
+		TouchMenuTest(&dev, fx24G, CONFIG_WIDTH, CONFIG_HEIGHT, 1000);
+		TouchMoveTest(&dev, fx24G, CONFIG_WIDTH, CONFIG_HEIGHT, 1000);
+#ifndef CONFIG_IDF_TARGET_ESP32S2
+	TouchIconTest(&dev, fx24G, CONFIG_WIDTH, CONFIG_HEIGHT, 1000);
+#endif
+#endif
+
+		WAIT;
+		char file[32];
+		strcpy(file, "/images/jugo.png"); //IMPORTER UNE IMAGE VERS LA MEMOIRE FLASH
+		PNGTest(&dev, file, CONFIG_WIDTH, CONFIG_HEIGHT); //AFFICHER L'IMAGE
+		WAIT;
+		WAIT;
+
+		// Multi Font Test
+		uint16_t color;
+		uint8_t ascii[40];
+		uint16_t margin = 10;
+		lcdFillScreen(&dev, BLACK);
+		color = WHITE;
+		lcdSetFontDirection(&dev, DIRECTION0);
+		uint16_t xpos =0 ;
+		uint16_t ypos = 90;
+		int xd = 0;
+		int yd = 1;
+	
+			xpos = xpos - (32 * xd) - (margin * xd);; //POSITION X DU TEXTE A AFFICHER 
+			ypos = ypos + (24 * yd) + (margin * yd); //POSITION Y DU TEXTE A AFFICHER
+			strcpy((char *)ascii, "Bonjour, ceci est un");
+			lcdDrawString(&dev, fx32G, xpos, ypos, ascii, color);  //&dev:ECRAN SUR LEQUEL ON AFFICHE, fx32G: FONT, xpos/ypos: POSITION,ascii:TEXTE A AFFICHER, color:COULEUR
+			strcpy((char *)ascii, "ecran graphique");
+			lcdDrawString(&dev, fx32G, xpos+30, ypos+30, ascii, color); //xpos+30 ypos+30: PREMIERE LIGNE OCCUPE TOUT L'ECRAN DEUXIEME LIGNE EST EN DESSOUS (+30)
+		
+		lcdSetFontDirection(&dev, DIRECTION0); //DIRECTION
+		WAIT;
+		WAIT;
+	} // end while
+}
+//MEMOIRE FLASH PAS IMPORTANT A COMPRENDRE
+static void listSPIFFS(char * path) {
+	DIR* dir = opendir(path);
+	assert(dir != NULL);
+	while (true) {
+		struct dirent*pe = readdir(dir);
+		if (!pe) break;
+		ESP_LOGI(__FUNCTION__,"d_name=%s d_ino=%d d_type=%x", pe->d_name,pe->d_ino, pe->d_type);
+	}
+	closedir(dir);
+}
+//SYSTEME SPIFFS POUR ENREGISTRER LES FICHIERS/ IMAGES. PAS IMPORTANT A COMPRENDRE
+esp_err_t mountSPIFFS(char * path, char * label, int max_files) {
+	esp_vfs_spiffs_conf_t conf = {
+		.base_path = path,
+		.partition_label = label,
+		.max_files = max_files,
+		.format_if_mount_failed =true
+	};
+	esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+	if (ret != ESP_OK) {
+		if (ret ==ESP_FAIL) {
+			ESP_LOGE(TAG, "Failed to mount or format filesystem");
+		} else if (ret== ESP_ERR_NOT_FOUND) {
+			ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+		} else {
+			ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)",esp_err_to_name(ret));
+		}
+		return ret;
+	}
+
+
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info(conf.partition_label, &total, &used);
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG,"Failed to get SPIFFS partition information (%s)",esp_err_to_name(ret));
+	} else {
+		ESP_LOGI(TAG,"Mount %s to %s success", path, label);
+		ESP_LOGI(TAG,"Partition size: total: %d, used: %d", total, used);
+	}
+
+	return ret;
+}
+
+
+void app_main()
+{
+	// INITIALISER NVS
+	ESP_LOGI(TAG, "Initialize NVS");
+	esp_err_t err = nvs_flash_init();
+	if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		err = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK( err );
+
+
+
+	//INITIALISER SPIFFS OU LES IMAGES SONT ENREGISTREES, 3 ESPACE, /icons ET /images SONT OU LES IMAGES SERONT ENREGISTREES
+	ESP_LOGI(TAG, "Initializing SPIFFS");
+	esp_err_t ret;
+	ret = mountSPIFFS("/spiffs", "storage0", 10);
+	if (ret != ESP_OK) return;
+	listSPIFFS("/spiffs/");
+
+	ret = mountSPIFFS("/icons", "storage1", 10);
+	if (ret != ESP_OK) return;
+	listSPIFFS("/icons/");
+
+	ret = mountSPIFFS("/images", "storage2", 14);
+	if (ret != ESP_OK) return;
+	listSPIFFS("/images/");
+
+
+	//APPELER LA FONCTION PRINCIPALE, OU L'INITIALISATION DE L'ECRAN ET TOUTE AUTRE FONCTION DONT ON A PARLER
+
+	xTaskCreate(TFT, "TFT", 1024*6, NULL, 2, NULL);
+
+}
